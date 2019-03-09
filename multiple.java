0b3cd8067324ccdc288e25b2712a3b508bcd61ec
sarika998 <47376634+sarika998@users.noreply.github.com>
2019-03-09 03:14:16 -0800

    Create multiple.java

---
+++ multiple.java
@@ -0,0 +1,23 @@
+ import java.util.*;
+ class HW5Problem3 {
+   public static void main (String [] args) {
+
+      int i = 0;
+      int n = 0;
+
+      Scanner input = new Scanner(System.in);
+
+         System.out.print("Enter i: ");
+         i = input.nextInt();
+         System.out.print("Enter n: ");
+         n = input.nextInt();
+
+         if ((i <= 1) || (n <= 1)) {
+            System.out.println("Please enter numbers above 1");
+            System.exit(1);
+         }
+
+         System.out.print(n + " multiples of " + i + " are: ");
+
+   }
+}
